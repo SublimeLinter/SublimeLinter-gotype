59c343427c715a6b00e3effe8d998747ef225404
SirReal <jon@surrell.es>
2014-01-20 15:31:30 +0100

    Gotype linting

---
+++ linter.py
@@ -0,0 +1,24 @@
+#
+# linter.py
+# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
+#
+# Written by Jon Surrell
+# Copyright (c) 2014 Jon Surrell
+#
+# License: MIT
+#
+
+"""This module exports the Gotype plugin class."""
+
+from SublimeLinter.lint import Linter, util
+
+
+class Gotype(Linter):
+
+    """Provides an interface to gotype."""
+
+    syntax = 'go'
+    cmd = ('gotype', '-e')
+    regex = r'^.+:(?P<line>\d+):(?P<col>\d+):\s+(?P<message>.+)$'
+    tempfile_suffix = 'go'
+    error_stream = util.STREAM_STDERR
