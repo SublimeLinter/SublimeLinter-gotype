14c473b8bef44ee5b521ce365ad89249c7f6e39e
SirReal <jon.surrell@gmail.com>
2014-01-21 11:12:29 +0100

    Remove trailing $ from regex

--- linter.py
+++ linter.py
@@ -19,6 +19,6 @@ class Gotype(Linter):
 
     syntax = 'go'
     cmd = ('gotype', '-e')
-    regex = r'^.+:(?P<line>\d+):(?P<col>\d+):\s+(?P<message>.+)$'
+    regex = r'^.+:(?P<line>\d+):(?P<col>\d+):\s+(?P<message>.+)'
     tempfile_suffix = 'go'
     error_stream = util.STREAM_STDERR
