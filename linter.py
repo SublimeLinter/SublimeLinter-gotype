c1fff2ae86f03158cd9e29440c523ff262f6f713
jaschaephraim <jascha@jaschaephraim.com>
2014-07-11 06:28:21 -0700

    Add custom GOPATH setting

--- linter.py
+++ linter.py
@@ -24,6 +24,17 @@ class Gotype(Linter):
     regex = r'^.+:(?P<line>\d+):(?P<col>\d+):\s+(?P<message>.+)'
     error_stream = util.STREAM_STDERR
 
+    def __init__(self, view, syntax):
+        """Initialize and load GOPATH from settings if present."""
+        super(Gotype, self).__init__(view, syntax)
+
+        gopath = self.get_view_settings().get('gopath')
+        if gopath:
+            if self.env:
+                self.env['GOPATH'] = gopath
+            else:
+                self.env = {'GOPATH': gopath}
+
     def run(self, cmd, code):
         """Copy package files to temp dir."""
         files = [f for f in listdir(dirname(self.filename)) if f[-3:] == '.go']
