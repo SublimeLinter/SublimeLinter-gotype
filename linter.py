ff41c41a15fb095580f9a6241a497dae624bfc09
Tyler Bunnell <tylerbunnell@gmail.com>
2015-02-15 08:43:12 -0700

    Change linter to work with live files

--- linter.py
+++ linter.py
@@ -10,24 +10,41 @@
 
 """This module exports the Gotype plugin class."""
 
-from os import listdir
+from os import path
 from os.path import dirname
 from SublimeLinter.lint import Linter, util
+import re
 
 
-class Gotype(Linter):
+class GoType(Linter):
 
     """Provides an interface to gotype."""
 
     syntax = ('go', 'gosublime-go')
-    cmd = ('gotype', '-e', '-a', '.')
+    cmd = None
+    executable = 'gotype'
     regex = r'^.+:(?P<line>\d+):(?P<col>\d+):\s+(?P<message>.+)'
+    fileRegex = r'^(?P<file>[^:]+):.+'
     error_stream = util.STREAM_STDERR
 
-    def __init__(self, view, syntax):
-        """Initialize and load GOPATH from settings if present."""
-        super(Gotype, self).__init__(view, syntax)
+    def split_match(self, match):
+        """
+        Extract and return values from match.
 
+        We override this method so that errors not pertaining to the file
+        being edited are not returned.
+
+        """
+
+        match, line, col, error, warning, message, near = super().split_match(match)
+
+        filename = path.basename(self.filename)
+        if re.match(self.fileRegex,match.group(0)).group(1) != filename:
+            return None, 0, 0, '', '', '', ''
+
+        return match, line, col, error, warning, message, near
+
+    def cmd(self):
         gopath = self.get_view_settings().get('gopath')
         if gopath:
             if self.env:
@@ -35,7 +52,6 @@ class Gotype(Linter):
             else:
                 self.env = {'GOPATH': gopath}
 
-    def run(self, cmd, code):
-        """Copy package files to temp dir."""
-        files = [f for f in listdir(dirname(self.filename)) if f[-3:] == '.go']
-        return self.tmpdir(cmd, files, code)
+        """Generate the linter command string"""
+        return [self.executable_path, '-e', '-a', '-r='+path.basename(self.filename), '.']
+
