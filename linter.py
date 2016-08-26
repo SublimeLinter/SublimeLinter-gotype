775e50cd9a67b2bcc0216f8c256145040cf3e2a4
Giovanni Bajo <rasky@develer.com>
2016-08-27 01:35:58 +0200

    Run gotype on directories, and add vendor support

--- linter.py
+++ linter.py
@@ -20,7 +20,7 @@ class Gotype(Linter):
     """Provides an interface to gotype."""
 
     syntax = ('go', 'gosublime-go')
-    cmd = ('gotype', '-e', '-a', '.')
+    executable = "gotype"
     regex = r'^.+:(?P<line>\d+):(?P<col>\d+):\s+(?P<message>.+)'
     error_stream = util.STREAM_STDERR
 
@@ -35,7 +35,5 @@ class Gotype(Linter):
             else:
                 self.env = {'GOPATH': gopath}
 
-    def run(self, cmd, code):
-        """Copy package files to temp dir."""
-        files = [f for f in listdir(dirname(self.filename)) if f[-3:] == '.go']
-        return self.tmpdir(cmd, files, code)
+    def cmd(self):
+        return [self.executable_path, "-e", "-a", dirname(self.filename)]
