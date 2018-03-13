058becb954622b3ca7a9570827de076503e7e393
Wonyoung Ju <getogrand@gmail.com>
2018-03-13 16:57:35 +0900

    Make it compatible with SublimeLinter4

--- linter.py
+++ linter.py
@@ -5,37 +5,55 @@
 # Written by Jon Surrell
 # Copyright (c) 2014 Jon Surrell
 #
+# Wonyoung Ju make it compatible with SublimeLinter4 at 2018.03
+#
 # License: MIT
 #
 
-"""This module exports the Gotype plugin class."""
+"""This module exports the GotypeLinter plugin class."""
 
+import os
 from os.path import dirname
-from SublimeLinter.lint import Linter, util
+import logging
+
+from SublimeLinter.lint import Linter, util  # or NodeLinter, PythonLinter, ComposerLinter, RubyLinter
 
 
-class Gotype(Linter):
+logger = logging.getLogger(__name__)
+
+
+class GotypeLinter(Linter):
     """Provides an interface to gotype."""
 
     syntax = ('go', 'gosublime-go')
     executable = "gotype"
     regex = r'(?P<filename>^.+):(?P<line>\d+):(?P<col>\d+):\s+(?P<message>.+)'
-    error_stream = util.STREAM_STDERR
-
-    def __init__(self, view, syntax):
-        """Initialize and load GOPATH from settings if present."""
-        super(Gotype, self).__init__(view, syntax)
-
-        gopath = self.get_view_settings().get('gopath')
-        if gopath:
-            if self.env:
-                self.env['GOPATH'] = gopath
-            else:
-                self.env = {'GOPATH': gopath}
 
     def cmd(self):
         """Return the command line to run."""
         return [self.executable_path, "-e", dirname(self.filename)]
+
+    def communicate(self, cmd, code=None):
+        """Customize Linter.communicate for pulling unnecessary file name off."""
+        settings = self.get_view_settings()
+        cwd = self.get_working_dir(settings)
+        env = self.get_environment(settings)
+
+        if logger.isEnabledFor(logging.INFO):
+            logger.info('{}: {} {}'.format(
+                self.name,
+                os.path.basename(self.filename or '<unsaved>'),
+                cmd)
+            )
+            if cwd:
+                logger.info('{}: cwd: {}'.format(self.name, cwd))
+
+        return util.communicate(
+            cmd,
+            code,
+            output_stream=self.error_stream,
+            env=env,
+            cwd=cwd)
 
     def split_match(self, match):
         """Process each match modifying or discarding it."""
