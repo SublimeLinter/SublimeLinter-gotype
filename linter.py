5ed938b8995c67b0a4f6ebeed8ed86ac37af42b5
Koen Lageveen <koen.lageveen@me.com>
2018-04-02 20:45:00 +0200

    remove executable and executable_path

--- linter.py
+++ linter.py
@@ -1,29 +1,13 @@
-from os.path import dirname
 from SublimeLinter.lint import Linter, util
 
 
 class Gotype(Linter):
-    executable = "gotype"
+    cmd = ('gotype', '-e', '${file}')
     regex = r'(?P<filename>^.+):(?P<line>\d+):(?P<col>\d+):\s+(?P<message>.+)'
     error_stream = util.STREAM_STDERR
     defaults = {
         'selector': 'source.go'
     }
-
-    def __init__(self, view, syntax):
-        """Initialize and load GOPATH from settings if present."""
-        super(Gotype, self).__init__(view, syntax)
-
-        gopath = self.get_view_settings().get('gopath')
-        if gopath:
-            if self.env:
-                self.env['GOPATH'] = gopath
-            else:
-                self.env = {'GOPATH': gopath}
-
-    def cmd(self):
-        """Return the command line to run."""
-        return [self.executable_path, "-e", dirname(self.filename)]
 
     def split_match(self, match):
         """Process each match modifying or discarding it."""
